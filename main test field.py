e8b3a3abe680760ec2d011be85fa9a8ea365fed0
MARVRK <bfg578@gmail.com>
2024-01-09 21:45:04 +0100

    new 2

---
+++ main test field.py
@@ -0,0 +1,22 @@
+# # print("hello word")
+# # print ('else')
+
+
+# numbers="DICK, 1, 3, 5, 8, 4, 5, 3 ,6 ,7 ,8 ,7, 2, 2 ,1"
+# odd_numbers = numbers[:5]
+# print(odd_numbers)
+name = input("type your name qrwa :")
+surname = input("and surname also: ")
+
+age = input("and your oldfuck age please: ")
+############################################## ----> code to make exceptacne in terminal
+try:
+    age = int(age)
+    if age >= int(age):
+            print(age)                     
+except ValueError:
+    print(f'{age}you should write the number, idiot...')   
+
+##############################################
+
+
