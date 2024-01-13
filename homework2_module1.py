963101c29d7ea5b199369c635e3bb7aff0e33abb
MARVRK <bfg578@gmail.com>
2024-01-13 13:24:52 +0100

    Create homework2_module1.py

---
+++ homework2_module1.py
@@ -0,0 +1,13 @@
+x=int(input(""))
+y=int(input(""))
+
+if x >= 0:
+    if y >= 0:  # x > 0, y > 0
+        print("Перша чверть")
+    else:  # x > 0, y < 0
+        print("Четверта чверть")
+else:
+    if y >= 0:  # x < 0, y > 0
+        print("Друга чверть")
+    else:  # x < 0, y < 0
+        print("Третя чверть")
