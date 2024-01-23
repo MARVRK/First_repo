5d91b71b54ae60e221c4c9479c29f225b7eb5891
MARVRK <155836501+MARVRK@users.noreply.github.com>
2024-01-23 15:59:40 +0100

    v2

--- goit-algo-hw-03/homework_module3_task1.py
+++ goit-algo-hw-03/homework_module3_task1.py
@@ -1,4 +1,4 @@
-# ################################################ <---- TASK 1
+
 from datetime import datetime
 
 def get_days_from_today(date: str):
@@ -6,7 +6,8 @@ def get_days_from_today(date: str):
         date = datetime.fromisoformat(date)
         today_date= datetime.today()
         total= int((today_date-date).days)
-        print(f"{total} days")
+        return total
+    
     except ValueError:
             print("Invalid ISOFormat , please use format YYYY-MM-DD")
     
@@ -14,6 +15,7 @@ def get_days_from_today(date: str):
   
 provide_date="2024-01-23"   
 to_summon_function=get_days_from_today(provide_date)
+print(f"{to_summon_function} days")
     
 ################################################### <----- Please check alternative solution for Task 1
 
@@ -21,16 +23,16 @@ to_summon_function=get_days_from_today(provide_date)
 
 # def get_days_from_today(date_is, format_is):
 #     date=datetime.strptime(date_is, format_is)
-#     today=datetime.today()
-#     print(((date-today).days),"days")
+#     today_date= datetime.today()
+#     total= int((today_date-date).days)
+#     print(f"{total} days")
     
 # while True:
 #         try:   
-#             date_is=input("Write date with '.' spacing format: ")
-#             format_is="%Y.%m.%d"
+#             date_is=input("Write date with '-' spacing format: ")
+#             format_is="%Y-%m-%d"
 #             to_summon_function=get_days_from_today(date_is,format_is)
 #         except ValueError :
-#             print(f"date_is {date_is} does not match format YYYY.MM.DD")
+#             print(f"date_is {date_is} does not match ISOformat YYYY-MM-DD")
 #         break
 
-# ################################################ <---- TASK 2
